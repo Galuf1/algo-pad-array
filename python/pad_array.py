4847283dfc94d6135259d2c271ba2e9e4d443229
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-30 15:15:04 +0200

    python done

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,25 @@
 #REMEMBER TO PSEUDOCODE
-def pad(array, min_size, value = None):
+def pad(array, min_size, value = None): 
+    size = min_size - len(array)
+    result = []
+    if size <= 0:
+        return array
+    else: result = [value] * min_size
+    for i,j in enumerate(array):
+        result[i] = j
+    return result
+
+
+
+
+
+
+
+
+#       pseudocode
+
+# first we need to obtain the input arrays length, and compare it with the min size. If it's smaller return the original array.
+
+# second we create a result array of the size asked in the input filled with the value asked.
+
+# last we replace the result values with the input array ones.
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,6 @@
 # Write unit tests!
+from pad_array import pad
+
+print(pad([1,2,3], 5, 'apple')==[1,2,3,'apple', 'apple'])
+print(pad([1,2,3], 3, 'apple')==[1,2,3])
+print(pad([1,2,3], 5)==[1,2,3,None,None])
