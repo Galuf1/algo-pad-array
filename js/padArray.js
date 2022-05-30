df65ee247d8d1a136c4256a11980f666ce1dbff4
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-30 15:40:43 +0200

    js done

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,19 @@
 // REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
-
+exports.padArray = (array, minSize, value=null) => {
+    const diff = minSize - array.length
+    if (diff <= 0){
+        return array
+    }
+    result = new Array(minSize).fill(value)
+    
+    for (let i = 0; i < array.length; i++){
+        result[i] = array[i]
+    }
+    
+    return result
 }
+/* first get the size of the array versus the minSize, if it's under 1 return the input array
+
+second create an array of the minSize length filled with the value asked
+
+last we assing the values of the input array into the result array*/
--- js/padArraySpec.js
+++ js/padArraySpec.js
@@ -1,1 +1,7 @@
 // Write unit tests!
+var shallowEqualArrays = require("shallow-equal/arrays")
+var pad = require("./padArray")
+
+console.log(shallowEqualArrays(pad.padArray([1,2,3],5,"apple"),[1,2,3,"apple","apple"]))
+console.log(shallowEqualArrays(pad.padArray([1,2,3],3,"apple"),[1,2,3]))
+console.log(shallowEqualArrays(pad.padArray([1,2,3],5),[1,2,3,null,null]))
